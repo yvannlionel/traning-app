fb16d3112008f99d3e4b73048c8d8d204ad2337a
yvannlionel <67588744+yvannlionel@users.noreply.github.com>
2020-06-30 14:05:30 +0200

    Add files via upload

---
+++ App3.java
@@ -0,0 +1,12 @@
+public class App3
+{
+ public void fn()
+{
+}
+private void fn2() {}
+private void fn1() 
+{ }
+private void fn3()
+{
+}
+}
---
+++ App4.java
@@ -0,0 +1,14 @@
+public class App4
+{
+private fn1()
+{
+}
+private void fn2()
+{
+}
+private void fn3()
+{
+}
+
+}
+
